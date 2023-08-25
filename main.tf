c36406db85efd7a3380b6aa41906017a825d9cc4
Björn Petersen <git@bjoernpetersen.net>
2023-08-25 12:10:19 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = "1.5.6"
+
+  backend "gcs" {
+    bucket = "prep-terraform-state"
+    prefix = "woog-life"
+  }
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+    google = {
+      version = "~> 4.79.0"
+    }
+  }
+}
+
+provider "github" {
+  owner = "woog-life"
+}
+
+provider "google" {
+  project = "prep-telegram-bots"
+  region  = "europe-west3"
+}
