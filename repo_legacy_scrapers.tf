3c1ad2834e31776f8d87efa7f5cf267d85880b3d
Björn Petersen <git@bjoernpetersen.net>
2023-08-25 14:02:38 +0200

    Archive legacy repos, enable actions

--- repo_legacy_scrapers.tf
+++ repo_legacy_scrapers.tf
@@ -6,19 +6,28 @@ module "scraper_repo" {
   is_archived            = true
 }
 
-#module "legacy_scraper_repos" {
-#  for_each = toset([
-#    "aare-scraper",
-#    "alster-scraper",
-#    "booking-scraper",
-#    "potsdam-booking-scraper",
-#    "potsdam-scraper",
-#    "tourlogger-scraper",
-#  ])
-#  source              = "github.com/BlindfoldedSurgery/terraform-repo-module"
-#  name                = each.key
-#  default_branch_name = "master"
-#  enable_actions      = false
-#  required_status_checks = []
-#  # TODO archive
-#}
+module "booking_scraper_repo" {
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module"
+  name                   = "booking-scraper"
+  enable_actions         = false
+  required_status_checks = []
+  is_archived            = true
+}
+
+module "legacy_scraper_repos" {
+  for_each = toset([
+    "aare-scraper",
+    "alster-scraper",
+    "potsdam-booking-scraper",
+    "potsdam-scraper",
+    "tourlogger-scraper",
+  ])
+
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module"
+  name                   = each.key
+  default_branch_name    = "master"
+  blocked_branches       = []
+  enable_actions         = false
+  required_status_checks = []
+  is_archived            = true
+}
--- this_repo.tf
+++ this_repo.tf
@@ -7,7 +7,7 @@ module "self_repo" {
     "plan",
   ]
   description    = "Organization infrastructure, including GitHub repositories"
-  enable_actions = false
+  enable_actions = true
 }
 
 resource "google_service_account" "self" {
