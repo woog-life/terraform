92a6d7b1089ab0ac977d7822b11836d5cb1af741
Torben Carstens <git@carstens.tech>
2023-12-05 22:01:05 +0100

    Enable actions in `graphing`

--- repo_graphing.tf
+++ repo_graphing.tf
@@ -2,7 +2,7 @@ module "graphing_repo" {
   source              = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v5.0.0"
   name                = "graphing"
   description         = "Creates history graphs for all lakes and uploads them to S3"
-  enable_actions      = false
+  enable_actions      = true
   default_branch_name = "main"
   blocked_branches    = []
   required_status_checks = [
